b845460b4d054acdedb3fb22adac9d04bbbfe9e7
MultisampledNight <contact@multisamplednight.com>
2024-11-03 05:17:03 +0100

    feat: automatically apply migrations on startup

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,27 @@
 pub mod entity;
 
 use eyre::Result;
-use sea_orm::Database;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::{ActiveModelTrait, ActiveValue, Database, EntityTrait};
+use time::macros::datetime;
 
 #[tokio::main]
 pub async fn run() -> Result<()> {
     let db = "sqlite:./track.db?mode=rwc";
     let db = Database::connect(db).await?;
+
+    Migrator::up(&db, None).await?;
+
+    let purchase = entity::product::ActiveModel {
+        name: ActiveValue::set(Some("mate".to_string())),
+        default_price: ActiveValue::set(500),
+        created_at: ActiveValue::set(datetime!(2024-10-03 05:07)),
+        ..Default::default()
+    };
+
+    purchase.insert(&db).await?;
+
+    dbg!(entity::product::Entity::find().all(&db).await?);
 
     Ok(())
 }
