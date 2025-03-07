288d2e50e3db97dac1f8269bca1a51fa911e2d64
MultisampledNight <contact@multisamplednight.com>
2025-03-07 02:41:17 +0100

    factor: remove database for now
    With the model I'm going for at the moment, there's no purpose in
    keeping a database, given that all state there is is kept in scripts.

--- src/config.rs
+++ src/config.rs
@@ -3,15 +3,11 @@ use clap::Parser;
 #[derive(Parser, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[command(version, about, author)]
 pub struct Config {
-    #[arg(short, long, default_value_t = Self::default().database)]
-    pub database: String,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
-            // TODO: make this configurable + depend on e.g. $XDG_STATE_HOME instead
-            database: "sqlite:./track.db?mode=rwc".to_string(),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
 pub mod config;
 pub mod model;
+pub mod ui;
 
 use eyre::Result;
-use migration::{Migrator, MigratorTrait};
-use sea_orm::Database;
 
 #[tokio::main]
 pub async fn run() -> Result<()> {
-    let cfg = config::cli();
-    let db = Database::connect(cfg.database).await?;
-
-    Migrator::up(&db, None).await?;
+    let _cfg = config::cli();
 
     Ok(())
 }
