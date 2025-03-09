7a37617317c4f3aa982d608bd467817f362ab1da
MultisampledNight <contact@multisamplednight.com>
2025-03-09 01:24:46 +0100

    refactor: remove parser helper macro, switch to trait instead

--- src/lang/parse.rs
+++ src/lang/parse.rs
@@ -46,27 +46,18 @@ macro_rules! param {
     };
 }
 
-macro_rules! parser {
-    ($(
-        $( #[$attr:meta] )*
-        $vis:vis fn $fn_name:ident
-        ($($param_name:ident : $param_type:ty),* $(,)?)
-        -> $ret:ty
-        { $body:expr }
-    )*) => {$(
-        $( #[$attr] )*
-        fn $fn_name<'a>($( $param_name : $param_type ),*)
-            -> impl Parser<'a, &'a str, $ret, Ctx<'a>>
-        {
-            $body
-        }
-    )*};
-}
-
 /// Returns the contained element for a single-element tuple.
 fn untup<T>((ele,): (T,)) -> T {
     ele
 }
+
+/// Shorthand for the Parser trait.
+pub trait P<'a, Node>: Parser<'a, &'a str, Node, Ctx<'a>> {}
+impl<'a, Node, T> P<'a, Node> for T where T: Parser<'a, &'a str, Node, Ctx<'a>> {}
+
+/// Shorthand for `P<'a, ()>`.
+trait E<'a>: P<'a, ()> {}
+impl<'a, T> E<'a> for T where T: P<'a, ()> {}
 
 // complexity rises the further down
 // read the return types as "what will *the parser returned by this function* return"
@@ -78,212 +69,202 @@ fn untup<T>((ele,): (T,)) -> T {
 //
 // - for calming the lsp while writing new parsers, consider appending
 //   `.ignore_then(todo())`
-parser! {
-    // basics
 
-    /// Hard/necessary inline whitespace.
-    fn hsp() -> () {
-        inline_whitespace().at_least(1)
-    }
+// basics
 
-    /// Optional inline whitespace.
-    fn osp() -> () {
-        inline_whitespace()
-    }
+/// Hard/necessary inline whitespace.
+fn hsp<'a>() -> impl E<'a> {
+    inline_whitespace().at_least(1)
+}
 
-    fn ident() -> Ident {
-        chumsky::text::ident().map(Ident::new)
-    }
+/// Optional inline whitespace.
+fn osp<'a>() -> impl E<'a> {
+    inline_whitespace()
+}
 
-    fn nat() -> Natural {
-        int(10)
-            .map(str::parse)
-            // expecting the int parser to only accept valid ints
-            .unwrapped()
-    }
+fn ident<'a>() -> impl P<'a, Ident> {
+    chumsky::text::ident().map(Ident::new)
+}
 
-    // literals
+fn nat<'a>() -> impl P<'a, Natural> {
+    int(10)
+        .map(str::parse)
+        // expecting the int parser to only accept valid ints
+        .unwrapped()
+}
 
-    fn gtin() -> Gtin {
-        todo()
-    }
+// literals
 
-    fn cents() -> Money {
-        nat()
-            .then_ignore(
-                osp()
-                    .then(choice((just("cents"), just("ct"), just("¢"))))
-                    .or_not(),
-            )
-            .map(Money)
-    }
+fn gtin<'a>() -> impl P<'a, Gtin> {
+    todo()
+}
 
-    fn euros() -> Money {
-        todo()
-    }
-
-    fn money() -> Money {
-        choice((cents(), euros()))
-    }
-
-    // parameters
-
-    fn from() -> Ident {
-        cmd!("from" ident()).map(untup)
-    }
-
-    fn to() -> Ident {
-        cmd!("to" ident()).map(untup)
-    }
-
-    fn dir() -> Dir {
-        group((from(), hsp(), to())).map(|(from, _, to)| Dir { from, to })
-    }
-
-    fn product() -> Product {
-        choice((
-            ident().map(Product::Name),
-            gtin().map(Product::Id),
-        ))
-    }
-
-    fn value() -> Money {
-        money()
-    }
-
-    fn price() -> Money {
-        cmd!("price" money()).map(untup)
-    }
-
-    // actors
-
-    fn entity() -> Entity {
-        cmd!("entity" ident())
-            .map(|(name,)| Entity { name })
-    }
-
-    fn object() -> Object {
-        cmd!(
-            "object"
-            ident(),
-            cmd!("parent" ident()).map(untup) => Parser::or_not,
+fn cents<'a>() -> impl P<'a, Money> {
+    nat()
+        .then_ignore(
+            osp()
+                .then(choice((just("cents"), just("ct"), just("¢"))))
+                .or_not(),
         )
-        .map(|(name, parent)| Object { name, parent })
-    }
+        .map(Money)
+}
 
-    fn concept() -> Concept {
-        cmd!(
-            "concept"
-            ident(),
-            price() => Parser::or_not,
-            cmd!("gtin" gtin()).map(untup) => Parser::or_not,
-        )
-        .map(|(name, default_price, gtin)| Concept {
-            name,
-            default_price,
-            gtin,
-        })
-    }
+fn euros<'a>() -> impl P<'a, Money> {
+    todo()
+}
 
-    fn actor() -> Actor {
-        choice((
-            entity().map(Actor::Entity),
-            object().map(Actor::Object),
-            concept().map(Actor::Concept),
-        ))
-    }
+fn money<'a>() -> impl P<'a, Money> {
+    choice((cents(), euros()))
+}
 
-    // commands
+// parameters
 
-    fn create() -> Create {
-        cmd!("create" actor())
-            .map(|(who,)| Create { who })
-    }
+fn from<'a>() -> impl P<'a, Ident> {
+    cmd!("from" ident()).map(untup)
+}
 
-    fn pay() -> Pay {
-        cmd!("pay" value(), dir())
-            .map(|(amount, who)| Pay { amount, who })
-    }
+fn to<'a>() -> impl P<'a, Ident> {
+    cmd!("to" ident()).map(untup)
+}
 
-    fn deliver() -> Deliver {
-        cmd!(
-            "deliver"
-            product(),
-            price() => Parser::or_not,
-            dir(),
-        )
-        .map(|(what, price, who)| Deliver { what, price, who })
-    }
+fn dir<'a>() -> impl P<'a, Dir> {
+    group((from(), hsp(), to())).map(|(from, _, to)| Dir { from, to })
+}
 
-    fn purchase() -> Purchase {
-        cmd!(
-            "purchase"
-            product(),
-            price() => Parser::or_not,
-            dir(),
-        )
-        .map(|(what, price, who)| Purchase { what, price, who })
-    }
+fn product<'a>() -> impl P<'a, Product> {
+    choice((ident().map(Product::Name), gtin().map(Product::Id)))
+}
 
-    fn stats() -> Stats {
-        choice((keyword("stats"), keyword("statistics"))).map(|_| Stats)
-    }
+fn value<'a>() -> impl P<'a, Money> {
+    money()
+}
 
-    fn balance() -> Balance {
-        choice((keyword("balance"), keyword("bal")))
-            .ignore_then(dir())
-            .map(|between| Balance { between })
-    }
+fn price<'a>() -> impl P<'a, Money> {
+    cmd!("price" money()).map(untup)
+}
 
-    fn transfer() -> Transfer {
-        choice((
-            pay().map(Transfer::Pay),
-            deliver().map(Transfer::Deliver),
-            purchase().map(Transfer::Purchase),
-        ))
-    }
+// actors
 
-    fn analyze() -> Analyze {
-        choice((
-            stats().map(Analyze::Stats),
-            balance().map(Analyze::Balance),
-        ))
-    }
+fn entity<'a>() -> impl P<'a, Entity> {
+    cmd!("entity" ident()).map(|(name,)| Entity { name })
+}
 
-    fn statement() -> Stmt {
-        choice((
-            create().map(Stmt::Create),
-            transfer().map(Stmt::Transfer),
-            analyze().map(Stmt::Analyze),
-        ))
-    }
+fn object<'a>() -> impl P<'a, Object> {
+    cmd!(
+        "object"
+        ident(),
+        cmd!("parent" ident()).map(untup) => Parser::or_not,
+    )
+    .map(|(name, parent)| Object { name, parent })
+}
 
-    // toplevel
+fn concept<'a>() -> impl P<'a, Concept> {
+    cmd!(
+        "concept"
+        ident(),
+        price() => Parser::or_not,
+        cmd!("gtin" gtin()).map(untup) => Parser::or_not,
+    )
+    .map(|(name, default_price, gtin)| Concept {
+        name,
+        default_price,
+        gtin,
+    })
+}
 
-    /// Upon a `#`, ignore everything until end of line or end of input.
-    fn comment() -> () {
-        just('#')
-            // What can appear in a comment?
-            // Lazy since `repeated` is greedy by default
-            // (would cause comments to include the next lines as well)
-            .then(any().repeated().lazy())
-            // How can comments be ended?
-            .then(choice((newline(), end())))
-            // Not modeled in the AST.
-            .ignored()
-    }
+fn actor<'a>() -> impl P<'a, Actor> {
+    choice((
+        entity().map(Actor::Entity),
+        object().map(Actor::Object),
+        concept().map(Actor::Concept),
+    ))
+}
 
-    fn delim() -> () {
-        choice((newline(), just(';').ignored())).padded()
-    }
+// commands
 
-    pub fn script() -> Script {
-        statement()
-            .padded_by(osp())
-            .separated_by(choice((delim(), comment())))
-            .allow_leading()
-            .allow_trailing()
-            .collect()
-            .map(Script)
-    }
+fn create<'a>() -> impl P<'a, Create> {
+    cmd!("create" actor()).map(|(who,)| Create { who })
+}
+
+fn pay<'a>() -> impl P<'a, Pay> {
+    cmd!("pay" value(), dir()).map(|(amount, who)| Pay { amount, who })
+}
+
+fn deliver<'a>() -> impl P<'a, Deliver> {
+    cmd!(
+        "deliver"
+        product(),
+        price() => Parser::or_not,
+        dir(),
+    )
+    .map(|(what, price, who)| Deliver { what, price, who })
+}
+
+fn purchase<'a>() -> impl P<'a, Purchase> {
+    cmd!(
+        "purchase"
+        product(),
+        price() => Parser::or_not,
+        dir(),
+    )
+    .map(|(what, price, who)| Purchase { what, price, who })
+}
+
+fn stats<'a>() -> impl P<'a, Stats> {
+    choice((keyword("stats"), keyword("statistics"))).map(|_| Stats)
+}
+
+fn balance<'a>() -> impl P<'a, Balance> {
+    choice((keyword("balance"), keyword("bal")))
+        .ignore_then(dir())
+        .map(|between| Balance { between })
+}
+
+fn transfer<'a>() -> impl P<'a, Transfer> {
+    choice((
+        pay().map(Transfer::Pay),
+        deliver().map(Transfer::Deliver),
+        purchase().map(Transfer::Purchase),
+    ))
+}
+
+fn analyze<'a>() -> impl P<'a, Analyze> {
+    choice((stats().map(Analyze::Stats), balance().map(Analyze::Balance)))
+}
+
+fn statement<'a>() -> impl P<'a, Stmt> {
+    choice((
+        create().map(Stmt::Create),
+        transfer().map(Stmt::Transfer),
+        analyze().map(Stmt::Analyze),
+    ))
+}
+
+// toplevel
+
+/// Upon a `#`, ignore everything until end of line or end of input.
+fn comment<'a>() -> impl E<'a> {
+    just('#')
+        // What can appear in a comment?
+        // Lazy since `repeated` is greedy by default
+        // (would cause comments to include the next lines as well)
+        .then(any().repeated().lazy())
+        // How can comments be ended?
+        .then(choice((newline(), end())))
+        // Not modeled in the AST.
+        .ignored()
+}
+
+fn delim<'a>() -> impl E<'a> {
+    choice((newline(), just(';').ignored())).padded()
+}
+
+pub fn script<'a>() -> impl P<'a, Script> {
+    statement()
+        .padded_by(osp())
+        .separated_by(choice((delim(), comment())))
+        .allow_leading()
+        .allow_trailing()
+        .collect()
+        .map(Script)
 }
