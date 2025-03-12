3e64c44593dd76cf64b76c4da6cd7099b7a7a5d6
MultisampledNight <contact@multisamplednight.com>
2025-03-12 21:17:15 +0100

    docs: cover how to add new commands

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,25 @@
 //! 2. Parse string into a [`Script`] using [`syntax::parse`].
 //!     - [`Script`] serves as the AST root
 //! 3. Run the [`Script`] in the [`Runtime`] using [`Runtime::run`]
+//!
+//! # Adding new commands
+//!
+//! Task list for adding a new command `nya`:
+//!
+//! 1. Syntax
+//!    a. Think of one (where is it used? what are its arguments?)
+//!    b. Update `doc/syntax.abnf` appropriately
+//!    c. Expand the AST in [`syntax::ast`]
+//!    to include `nya` below [`Stmt`][syntax::ast::Stmt]
+//!    in the tree
+//!    d. Parse it in [`syntax::parse`] (the `cmd!` macro is super useful here!)
+//! 2. Logic
+//!    a. Add a submodule `nya` in [`runtime::cmd`] for the command
+//!    b. In there, write a type `Nya` with the type-restricted arguments for the command
+//!    c. Enforce the semantic requirements between its AST and the command in [`runtime::repr`]
+//!    d. Write a method on [`Runtime`] that performs the op
+//!    e. Dispatch it in [`runtime::cmd`]
+//! 3. All done! Test, test, test!
 
 #[macro_use]
 extern crate macro_rules_attribute;
