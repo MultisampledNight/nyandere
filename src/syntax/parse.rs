62fb4830c80dd5b9b4f84041f4d6da3f60fea6f9
MultisampledNight <contact@multisamplednight.com>
2025-06-22 02:26:27 +0200

    refactor: clear out old AST remnants

--- src/syntax/parse.rs
+++ src/syntax/parse.rs
@@ -1,67 +1,25 @@
 #![allow(clippy::double_parens)]
 
-use chumsky::{
-    Parser,
-    prelude::*,
-    text::{digits, inline_whitespace, int, keyword, newline},
-};
+use chumsky::{Parser, prelude::*};
 
 use crate::ext::{Gtin, Money, Natural};
 
-use super::ast::*;
+use super::{ast::*, lex::Token};
 
 pub type Error<'a> = Rich<'a, char, SimpleSpan>;
 pub type Ctx<'a> = extra::Err<Error<'a>>;
 
-impl Script {
-    /// [`FromStr::from_str`] but not, since that doesn't allow lifetime restraints.
-    pub fn parse<'text>(source: &'text str) -> ParseResult<Self, Error<'text>> {
+impl<'src> Script<'src> {
+    /// [`FromStr::from_str`] but not, since that doesn't allow lifetime constraints.
+    pub fn parse(source: &'src str) -> ParseResult<Self, Error<'src>> {
+
         script().parse(source)
     }
 }
 
-/// Takes a command discriminant before the parens
-/// and arguments in the parens, returning a parser for it.
-/// *n* arguments lead to the return type of `(T_1, ..., T_n)`.
-macro_rules! cmd {
-    // split of 1 vs n is to avoid putting choice at all if there are no arguments
-    (
-        $($name:literal)|+ $( :
-            $arg_1:expr $(=> $arg_1_post:expr)?
-            $(, $arg_n:expr $(=> $arg_n_post:expr)? )* $(,)?
-        )?
-    ) => {
-        choice((
-            $(keyword($name),)*
-        ))
-        $(
-            .ignore_then(group((
-                param!($arg_1 $(=> $arg_1_post)?),
-                $(param!($arg_n $(=> $arg_n_post)?)),*
-            )))
-        )?
-    };
-}
-
-/// Prepends a hard space before the given argument,
-/// optionally with post-processing.
-macro_rules! param {
-    ($pre:expr $(=> $post:expr)?) => {
-        {
-            $(({ $post }))?
-            (hsp().ignore_then({ $pre }))
-        }
-    };
-}
-
-/// Returns the contained element for a single-element tuple.
-fn untup<T>((ele,): (T,)) -> T {
-    ele
-}
-
 /// Shorthand for the Parser trait.
-pub trait P<'a, Node>: Parser<'a, &'a str, Node, Ctx<'a>> {}
-impl<'a, Node, T> P<'a, Node> for T where T: Parser<'a, &'a str, Node, Ctx<'a>> {}
+pub trait P<'src, Node>: Parser<'src, Token<'src>, Node, Ctx<'src>> {}
+impl<'src, Node, T> P<'src, Node> for T where T: Parser<'src, Token<'src>, Node, Ctx<'src>> {}
 
 /// Shorthand for `P<'a, ()>`.
 pub trait E<'a>: P<'a, ()> {}
@@ -82,217 +40,63 @@ impl<'a, T> E<'a> for T where T: P<'a, ()> {}
 
 /// Hard/necessary inline whitespace.
 fn hsp<'a>() -> impl E<'a> {
-    inline_whitespace().at_least(1)
+    todo!()
 }
 
 /// Optional inline whitespace.
 fn osp<'a>() -> impl E<'a> {
-    inline_whitespace()
+    todo!()
 }
 
 pub fn ident<'a>() -> impl P<'a, Ident> {
-    chumsky::text::ident().map(Ident::new)
+    todo!()
 }
 
 pub fn nat<'a>() -> impl P<'a, Natural> {
-    int(10)
-        .from_str()
-        // expecting the int parser to only accept valid ints
-        .unwrapped()
+    todo!()
 }
 
 /// Allows exactly 2 fractional digits,
 /// returns them just left-shifted by 2 digits though
 /// (as if the dot was not there).
 pub fn decimal<'a>() -> impl P<'a, Natural> {
-    let two_digit_num = digits(10)
-        .exactly(2)
-        .collect()
-        .map(|src: String| src.parse::<Natural>())
-        // 2 base10 digits are always parsable as BigUint
-        .unwrapped();
-    nat()
-        .then(just('.').ignore_then(two_digit_num))
-        // left-shift
-        .map(|(whole, frac)| whole * 100u8 + frac)
+    todo!()
 }
 
 // literals
 
 pub fn gtin<'a>() -> impl P<'a, Gtin> {
-    digits(10)
-        .at_least(8)
-        .at_most(14)
-        .collect::<String>()
-        .from_str()
-        // GTINs can be at most 14 digits long, both max 14 and digits are fulfilled above
-        .unwrapped()
+    todo!()
 }
 
 pub fn ratio<'a>() -> impl P<'a, Ratio> {
-    group((
-        nat().then_ignore(osp().then(just(":"))),
-        osp().ignore_then(nat()),
-    ))
-    .map(|(left, right)| Ratio { left, right })
+    todo!()
 }
 
 pub fn cents<'a>() -> impl P<'a, Money> {
-    let suffix = osp()
-        .then(choice((just("cents"), just("ct"), just("¢"))))
-        .or_not();
-    nat().then_ignore(suffix).map(Money)
+    todo!()
 }
 
 pub fn euros<'a>() -> impl P<'a, Money> {
-    let value = choice((decimal(), nat().map(|value| value * 100u8)));
-    let suffix = osp().then(choice((just("EUR"), just("eur"), just("€"))));
-
-    value.then_ignore(suffix).map(Money)
+    todo!()
 }
 
 pub fn money<'a>() -> impl P<'a, Money> {
-    choice((euros(), cents()))
-}
-
-// parameters
-
-pub fn from<'a>() -> impl P<'a, Ident> {
-    cmd!("from" : ident()).map(untup)
-}
-
-pub fn to<'a>() -> impl P<'a, Ident> {
-    cmd!("to" : ident()).map(untup)
-}
-
-pub fn dir<'a>() -> impl P<'a, Dir> {
-    group((from(), hsp(), to())).map(|(from, _, to)| Dir {
-        source: from,
-        target: to,
-    })
-}
-
-pub fn product<'a>() -> impl P<'a, Product> {
-    choice((ident().map(Product::Name), gtin().map(Product::Id)))
-}
-
-pub fn price<'a>() -> impl P<'a, Money> {
-    cmd!("price" : money()).map(untup)
-}
-
-pub fn split<'a>() -> impl P<'a, Ratio> {
-    cmd!("split" : ratio()).map(untup)
-}
-
-// actors
-
-pub fn entity<'a>() -> impl P<'a, Entity> {
-    cmd!("entity" : ident()).map(|(name,)| Entity { name })
-}
-
-pub fn object<'a>() -> impl P<'a, Object> {
-    cmd!(
-        "object" :
-        ident(),
-        cmd!("parent" : ident()).map(untup) => Parser::or_not,
-    )
-    .map(|(name, parent)| Object { name, parent })
-}
-
-pub fn concept<'a>() -> impl P<'a, Concept> {
-    cmd!(
-        "concept" :
-        ident(),
-        price() => Parser::or_not,
-        cmd!("gtin" : gtin()).map(untup) => Parser::or_not,
-    )
-    .map(|(name, default_price, gtin)| Concept {
-        name,
-        default_price,
-        gtin,
-    })
-}
-
-pub fn actor<'a>() -> impl P<'a, Actor> {
-    choice((
-        entity().map(Actor::Entity),
-        object().map(Actor::Object),
-        concept().map(Actor::Concept),
-    ))
+    todo!()
 }
 
 // commands
 
-pub fn create<'a>() -> impl P<'a, Create> {
-    cmd!("create" : actor()).map(|(who,)| Create { who })
-}
-
-pub fn pay<'a>() -> impl P<'a, Pay> {
-    cmd!("pay" : money(), dir()).map(|(amount, who)| Pay { amount, who })
-}
-
-pub fn deliver<'a>() -> impl P<'a, Deliver> {
-    cmd!(
-        "deliver" :
-        product(),
-        price() => Parser::or_not,
-        dir(),
-        split() => Parser::or_not,
-    )
-    .map(|(what, price, who, split)| Deliver {
-        what,
-        price,
-        who,
-        split,
-    })
-}
-
-pub fn balance<'a>() -> impl P<'a, Balance> {
-    cmd!("balance" | "bal" : dir()).map(|(between,)| Balance { between })
-}
-
-pub fn transfer<'a>() -> impl P<'a, Transfer> {
-    choice((pay().map(Transfer::Pay), deliver().map(Transfer::Deliver)))
-}
-
-pub fn analyze<'a>() -> impl P<'a, Analyze> {
-    choice((balance().map(Analyze::Balance),))
-}
-
-pub fn statement<'a>() -> impl P<'a, Stmt> {
-    choice((
-        create().map(Stmt::Create),
-        transfer().map(Stmt::Transfer),
-        analyze().map(Stmt::Analyze),
-    ))
+pub fn statement<'a>() -> impl P<'a, Stmt<'a>> {
+    todo!()
 }
 
 // toplevel
 
-/// Upon a `#`, ignore everything until end of line or end of input.
-pub fn comment<'a>() -> impl E<'a> {
-    just('#')
-        // What can appear in a comment?
-        .then(any().and_is(newline().not()).repeated())
-        // How can comments be ended?
-        .then(choice((newline(), end())))
-        // Not modeled in the AST.
-        .ignored()
-}
-
 pub fn delim<'a>() -> impl E<'a> {
-    choice((comment(), newline(), just(';').ignored()))
-        .padded_by(osp())
-        .repeated()
-        .at_least(1)
+    todo!()
 }
 
-pub fn script<'a>() -> impl P<'a, Script> {
-    statement()
-        .separated_by(delim())
-        .allow_leading()
-        .allow_trailing()
-        .collect()
-        .then_ignore(end())
-        .map(Script)
+pub fn script<'src>() -> impl P<'src, Script<'src>> {
+    todo!()
 }
